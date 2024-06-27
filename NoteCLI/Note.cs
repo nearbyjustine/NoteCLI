021dd35d75b9a937e9a7b7943bb6027c078ed82d
Justine Castaneda <45316827+nearbyjustine@users.noreply.github.com>
2024-06-28 01:44:41 +0800

    added a Note class and a NoteManager class--will still complete the methods and properties for both.

---
+++ NoteCLI/Note.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteCLI
+{
+    internal class Note
+    {
+        private static int _nextId = 1;
+        private int _id;
+        private string _title;
+        private string _content;
+
+        public Note(string title, string content)
+        {
+            this.Id = _nextId;
+            this._title = title;
+            this._content = content;
+            _nextId++;
+        }
+
+        public string Title
+        {
+            get
+            {
+                return this._title;
+            }
+            set
+            {
+                this._title = value;
+            }
+        }
+        public string Content
+        {
+            get
+            {
+                return this._content;
+            }
+            set
+            {
+                this._content = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return this._id;
+            }
+            private set
+            {
+                this._id = value;
+            }
+        }
+    }
+}
---
+++ NoteCLI/NoteManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteCLI
+{
+    internal class NoteManager
+    {
+        private List<Note> notes;
+        public NoteManager() 
+        {
+            this.notes = new List<Note>();
+        }
+
+        public void AddNote(Note note)
+        {
+            // TODO: Add checking
+            notes.Add(note);
+        }
+        public void RemoveNote(int id)
+        {
+            // TODO: Add try catch for when note does not exist.
+            notes.Remove(notes.Single( n => n.Id == id));
+        }
+
+    }
+}
